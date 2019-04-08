ffd8e6fd2cb9627e59003f001ed872889c2b9f18
Matteo Nunez <matteo.nunez22@gmail.com>
2019-04-08 19:10:54 -0400

    Working g34_counter.vhd

--- lab2/.vhd and .vht/g34_counter.vhd
+++ lab2/.vhd and .vht/g34_counter.vhd
@@ -1,31 +1,26 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
 
 entity g34_counter is
 	port (	enable	: in	std_logic;
-				reset		: in	std_logic;
-				clk		: in	std_logic;
-				count		: out	std_logic_vector (3 downto 0));
+			reset	: in	std_logic;
+			clk		: in	std_logic;
+			count	: out	std_logic_vector (3 downto 0));
 end g34_counter;
 
-
 architecture a0 of g34_counter is
-
+	signal temp: std_logic_vector(3 downto 0);
 begin
-
-process (enable, reset, clk)
-begin
-	if (reset = '0') then	-- if reset is 0, sets output to 0
-		count <= "0000";
-	elseif ((enable = '1') and (rising_edge(clk)))	then	-- only counts up when enabled and on rising edge of clk
-		while (count < 10) loop										-- loops up to 10
-			count <= count + 1;
-		end loop;
-		
-		-- needs to reset somehow to 0 when count hits 10
-		
-	end if;
-end process;
-
+	process (enable, reset, clk)
+	begin
+		if(rising_edge(clk)) then	-- Only at the positive edge of 'Clk
+			if(reset = '0') then	-- 'temp' resets to 0000 only if 'reset' is 0
+				temp <= "0000";
+			elsif (enable = '1') then	-- 'temp' counts up only if 'enable' is 1
+				temp <= temp + "0001";	-- Adds 1 to 'temp'. If 'temp' = 1111, it becomes 0000
+			end if;
+		end if;
+	end process;
+	count <= temp;
 end a0;
